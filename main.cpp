96ee76cdf564e612556be6d4d1222e39c311e306
Nick Kinney <kinneyanick@gmail.com>
2020-02-11 16:49:53 -0600

    This might work

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 #include "perlin.h"
 
@@ -41,8 +42,8 @@ std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<int>> &
 int main(int argc, char** args){
     Perlin ng{std::vector<int>{10}};
     auto pos = std::vector<float>{0.12f, 0.13f, 2.3f};
-    for(float i =0; i < 3.0f; i+=0.01f){
-        std::cout << i << " :";
+    for(float i =0; i < 3.0f; i+=0.001f){
+        std::cout << std::fixed << std::setw(3) << std::setprecision(2) << std::setfill('0') <<  i << ":\t";
         pos.resize(0);
         pos.push_back(i);
         int temp = ng(pos)*20;
